4892d4363bf2b227c0af3ad895f935815333ad2a
Bob Jacobsen <jacobsen@mac.com>
2024-03-30 13:45:58 -0400

    add datagram stimulus

--- signal_generator.py
+++ signal_generator.py
@@ -3,6 +3,10 @@
 This uses the CAN link layer to generate
 messages for stimulating timing measurements.
 
+You should edit the `while` loop at the bottom
+to send the particular messages you'd like to exercise.
+There are some commented-out examples.
+
 Usage e.g.:
 python3.10 signal_generator.py -d /dev/cu.usbmodemCC570001B1
 
@@ -10,12 +14,11 @@ The -h option will display a full list of options.
 '''
 
 import sys
+import time
 
 from openlcb.nodeid import NodeID
 from openlcb.message import Message
 from openlcb.mti import MTI
-
-from queue import Empty
 
 def check():
     # set up the infrastructure
@@ -29,17 +32,38 @@ def check():
     # get configured DUT node ID - this uses Verify Global in some cases, but not all
     destination = olcbchecker.getTargetID()
 
-    # predefine messages
+    # predefine some messages
     snip = Message(MTI.Simple_Node_Ident_Info_Request, NodeID(olcbchecker.ownnodeid()), destination)
     pip = Message(MTI.Protocol_Support_Inquiry, NodeID(olcbchecker.ownnodeid()), destination)
+    datagramNull = Message(MTI.Datagram, NodeID(olcbchecker.ownnodeid()), destination, [00, 00])
+    datagramRead = Message(MTI.Datagram, NodeID(olcbchecker.ownnodeid()), destination, [0x20, 0x43, 0, 0, 0, 0, 2])
+    datagramAck  = Message(MTI.Datagram_Received_OK, NodeID(olcbchecker.ownnodeid()), destination)
     
-    import time
+    # wait for alias allocation to complete
+    time.sleep(2.000)
+    
     while True :
     
-    	olcbchecker.sendMessage(snip)
-    	olcbchecker.sendMessage(snip)
-    	#time.sleep(0.018)
-    	#olcbchecker.sendMessage(pip)
+        # SNIP and pip
+        #olcbchecker.sendMessage(snip)
+        #olcbchecker.sendMessage(snip)
+        #time.sleep(0.008)
+        #olcbchecker.sendMessage(snip)
+        #olcbchecker.sendMessage(pip)
+        
+        # Exercise datagrams 
+        # The delays before the responses are manually tuned for a specific node type
+    	olcbchecker.sendMessage(datagramRead)
+    	olcbchecker.sendMessage(datagramRead)
+    	olcbchecker.sendMessage(datagramRead)
+    	time.sleep(0.150)
+    	olcbchecker.sendMessage(datagramAck)
+    	time.sleep(0.100)
+    	olcbchecker.sendMessage(datagramAck)
+    	time.sleep(0.100)
+    	olcbchecker.sendMessage(datagramAck)
+    	
+    	# repeat periodically forever
     	time.sleep(3.000)
 		
 if __name__ == "__main__":
